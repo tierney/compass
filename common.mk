8c2c3e3079c07e18954c69ffa242aefeba74fb2a
Matt Tierney <tierney@cs.nyu.edu>
2013-06-17 14:22:01 -0400

    wip

--- common.mk
+++ common.mk
@@ -1,1 +1,2 @@
 AM_CXXFLAGS = -fPIC -Wall -Wno-sign-compare -Wno-format -std=gnu++11
+# AM_CXXFLAGS = -fPIC -std=gnu++11
--- prop_logic_main.cc
+++ prop_logic_main.cc
@@ -173,49 +173,51 @@ set<set<string>> Proposition::evaluate_all(const set<string>& variables) const
 
 int main() {
   assert(V("hello").evaluate({"hello"}));
+  // assert(("hello"_var).evaluate({"hello"}));
+  bool out = "hi"_var.evaluate({"hi"});
+  assert(out);
+    // assert(  ("foo"_var).evaluate({"foo"})); // trivially true
+    // assert(  ("foo"_var).evaluate_all({"foo"})
+    //          == set<set<string>> {{"foo"}} );
 
-    assert(  ("foo"_var) .evaluate({"foo"})); // trivially true
-    assert(  ("foo"_var) .evaluate_all({"foo"})
-             == set<set<string>> {{"foo"}} );
+    // assert(  (!"foo"_var) .evaluate({})); // basic negation
+    // assert(! (!"foo"_var) .evaluate({"foo"})); // basic negation
+    // assert(  (!"foo"_var) .evaluate_all({"foo"})
+    //          == set<set<string>> {{}} );
 
-    assert(  (!"foo"_var) .evaluate({})); // basic negation
-    assert(! (!"foo"_var) .evaluate({"foo"})); // basic negation
-    assert(  (!"foo"_var) .evaluate_all({"foo"})
-             == set<set<string>> {{}} );
+    // assert(  (!!"foo"_var) .evaluate({"foo"})); // double negation
+    // assert(  (!!"foo"_var) .evaluate_all({"foo"})
+    //          == set<set<string>> {{"foo"}} );
 
-    assert(  (!!"foo"_var) .evaluate({"foo"})); // double negation
-    assert(  (!!"foo"_var) .evaluate_all({"foo"})
-             == set<set<string>> {{"foo"}} );
+    // assert(  ("foo"_var && "bar"_var) .evaluate({"foo", "bar"})); // conjunction
+    // assert(! ("foo"_var && "bar"_var) .evaluate({"bar"})); // conjunction
+    // assert(! ("foo"_var && "bar"_var) .evaluate({"foo"})); // conjunction
+    // assert(! ("foo"_var && "bar"_var) .evaluate({})); // conjunction
+    // assert(  ("foo"_var && "bar"_var) .evaluate_all({"foo", "bar"})
+    //          == set<set<string>>({{"foo", "bar"}}));
 
-    assert(  ("foo"_var && "bar"_var) .evaluate({"foo", "bar"})); // conjunction
-    assert(! ("foo"_var && "bar"_var) .evaluate({"bar"})); // conjunction
-    assert(! ("foo"_var && "bar"_var) .evaluate({"foo"})); // conjunction
-    assert(! ("foo"_var && "bar"_var) .evaluate({})); // conjunction
-    assert(  ("foo"_var && "bar"_var) .evaluate_all({"foo", "bar"})
-             == set<set<string>>({{"foo", "bar"}}));
+    // assert(  ("foo"_var || "bar"_var) .evaluate({"foo", "bar"})); // disjunction
+    // assert(  ("foo"_var || "bar"_var) .evaluate({"bar"})); // disjunction
+    // assert(  ("foo"_var || "bar"_var) .evaluate({"foo"})); // disjunction
+    // assert(! ("foo"_var || "bar"_var) .evaluate({})); // disjunction
+    // assert(  ("foo"_var || "bar"_var) .evaluate_all({"foo", "bar"})
+    //          == set<set<string>>({{"foo", "bar"}, {"foo"}, {"bar"}}));
 
-    assert(  ("foo"_var || "bar"_var) .evaluate({"foo", "bar"})); // disjunction
-    assert(  ("foo"_var || "bar"_var) .evaluate({"bar"})); // disjunction
-    assert(  ("foo"_var || "bar"_var) .evaluate({"foo"})); // disjunction
-    assert(! ("foo"_var || "bar"_var) .evaluate({})); // disjunction
-    assert(  ("foo"_var || "bar"_var) .evaluate_all({"foo", "bar"})
-             == set<set<string>>({{"foo", "bar"}, {"foo"}, {"bar"}}));
+    // assert(  ("foo"_var.implies("bar"_var)) .evaluate({"foo", "bar"})); // implication
+    // assert(  ("foo"_var.implies("bar"_var)) .evaluate({"bar"})); // implication
+    // assert(! ("foo"_var.implies("bar"_var)) .evaluate({"foo"})); // implication
+    // assert(  ("foo"_var.implies("bar"_var)) .evaluate({})); // implication
+    // assert(  ("foo"_var.implies("bar"_var)) .evaluate_all({"foo", "bar"})
+    //          == set<set<string>>({{"foo", "bar"}, {"bar"}, {}}));
 
-    assert(  ("foo"_var.implies("bar"_var)) .evaluate({"foo", "bar"})); // implication
-    assert(  ("foo"_var.implies("bar"_var)) .evaluate({"bar"})); // implication
-    assert(! ("foo"_var.implies("bar"_var)) .evaluate({"foo"})); // implication
-    assert(  ("foo"_var.implies("bar"_var)) .evaluate({})); // implication
-    assert(  ("foo"_var.implies("bar"_var)) .evaluate_all({"foo", "bar"})
-             == set<set<string>>({{"foo", "bar"}, {"bar"}, {}}));
+    // assert(  ("foo"_var.iff("bar"_var)) .evaluate({"foo", "bar"})); // equivalence
+    // assert(! ("foo"_var.iff("bar"_var)) .evaluate({"bar"})); // equivalence
+    // assert(! ("foo"_var.iff("bar"_var)) .evaluate({"foo"})); //equivalence
+    // assert(  ("foo"_var.iff("bar"_var)) .evaluate({})); // equivalence
+    // assert(  ("foo"_var.iff("bar"_var)) .evaluate_all({"foo", "bar"})
+    //          == set<set<string>>({{"foo", "bar"}, {}}));
 
-    assert(  ("foo"_var.iff("bar"_var)) .evaluate({"foo", "bar"})); // equivalence
-    assert(! ("foo"_var.iff("bar"_var)) .evaluate({"bar"})); // equivalence
-    assert(! ("foo"_var.iff("bar"_var)) .evaluate({"foo"})); //equivalence
-    assert(  ("foo"_var.iff("bar"_var)) .evaluate({})); // equivalence
-    assert(  ("foo"_var.iff("bar"_var)) .evaluate_all({"foo", "bar"})
-             == set<set<string>>({{"foo", "bar"}, {}}));
-
-    cout << "((A and not B) implies C) and ((not A) iff (B and C)):" << endl << endl;
+    // cout << "((A and not B) implies C) and ((not A) iff (B and C)):" << endl << endl;
 
     // auto proposition = ("A"_var && !"B"_var).implies("C"_var) && (!"A"_var).iff("B"_var && "C"_var);
 
@@ -223,8 +225,8 @@ int main() {
         .implies("Q"_var && "S"_var);
     auto truth_assignments = proposition.evaluate_all({"P", "Q", "R", "S", "T"});
 
-    cout << "A    B    C" << endl;
-    cout << "-----------" << endl;
+    cout << "P    Q    R    S    T" << endl;
+    cout << "---------------------" << endl;
 
     for (auto truth_assignment : truth_assignments)
     {
